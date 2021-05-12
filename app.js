c6b7bccb457a405a90c00c4918c3c9a1abf4556a
Rajendra Khope <research@iocare.in>
2021-05-12 18:56:30 +0530

    Updated with notification

--- app.js
+++ app.js
@@ -1,112 +1,131 @@
-'use strict';
-const { app, BrowserWindow, Menu, Tray } = require('electron')
-var ipc = require('electron').ipcMain;
-var os = require('os');
-var {dialog} = require('electron');
-const { autoUpdater } = require('electron-updater');
-const path = require('path');
-//const AutoLaunch = require('auto-launch');
-let mainWindow;
+"use strict";
+
+var app = require("electron").app;
+var BrowserWindow = require("electron").BrowserWindow;
+
+const { Menu, Tray } = require("electron");
+var ipc = require("electron").ipcMain;
+//var os = require("os");
+//var { dialog } = require("electron");
+const { autoUpdater } = require("electron-updater");
+const path = require("path");
+const Store = require("electron-store");
+let store = null;
+let mainWindow = null;
 let tray = null;
 
+ipc.handle("get-version", (event, arg) => {
+  return app.getVersion();
+});
 
+ipc.handle("get-store", (event, arg) => {
+  console.log(arg);
+  return store.get(arg);
+});
 
-function createWindow () {
+ipc.on("set-store", (event, arg) => {
+  store.set(arg.key, arg.value);
+});
+
+function createWindow() {
+  autoUpdater.checkForUpdatesAndNotify();
+
   let win = new BrowserWindow({
-    width: 800,
+    width: 400,
     height: 600,
     autoHideMenuBar: true,
-    frame:false,
+    frame: false,
     resizable: false,
     center: true,
     webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
+      nodeIntegration: true,
+      contextIsolation: false,
     },
-    icon:"icons/playstore.png"
-  })
+    icon: path.join(__dirname, "icon.png"),
+  });
 
-  win.loadFile('index.html');
-  win.on('closed', function() {
-
+  win.loadFile("index.html");
+  win.on("closed", function () {
     mainWindow = null;
   });
 
   let tray = null;
-  win.on('minimize', function (event) {
-      event.preventDefault();
-      win.setSkipTaskbar(true);
-      tray = createTray();
+  win.on("minimize", function (event) {
+    event.preventDefault();
+    win.setSkipTaskbar(true);
+    tray = createTray();
   });
 
-  win.on('restore', function (event) {
-      win.show();
-      win.setSkipTaskbar(false);
-      tray.destroy();
+  win.on("restore", function (event) {
+    win.show();
+    win.setSkipTaskbar(false);
+    tray.destroy();
   });
-
 
   return win;
-
 }
 
 function createTray() {
-  let appIcon = new Tray(path.join(__dirname, "icons/playstore.png"));
+  let appIcon = new Tray(path.join(__dirname, "icon.png"));
   const contextMenu = Menu.buildFromTemplate([
-      {
-          label: 'Show', click: function () {
-              mainWindow.show();
-          }
+    {
+      label: "Show",
+      click: function () {
+        mainWindow.show();
       },
-      {
-          label: 'Exit', click: function () {
-              app.isQuiting = true;
-              app.quit();
-          }
-      }
+    },
+    {
+      label: "Exit",
+      click: function () {
+        app.isQuiting = true;
+        app.quit();
+      },
+    },
   ]);
 
-  appIcon.on('double-click', function (event) {
-      mainWindow.show();
+  appIcon.on("double-click", function (event) {
+    mainWindow.show();
   });
-  appIcon.setToolTip('Vaccinator');
+  appIcon.setToolTip("Vaccinator");
   appIcon.setContextMenu(contextMenu);
   return appIcon;
 }
 
 app.whenReady().then(() => {
-  mainWindow  = createWindow();
+  mainWindow = createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
-
-})
+  app.setAppUserModelId("in.iocare.vaccinator");
+  store = new Store();
+});
 
 app.setLoginItemSettings({
-    openAtLogin: true,
-    //path: electron.app.getPath("exe")
+  openAtLogin: true,
+  path: app.getPath("exe"),
 });
 
-ipc.on('window-status', (event, arg) => {
-    //event.sender.send("get-path", FFMPEG_PATH_EXTRACT + "/ffmpeg.exe")
-    if (arg==="minimise") {
-        mainWindow.minimize();
-    }
-    if (arg==="maximize") {
-        if (!mainWindow.isMaximized()) {
-            mainWindow.maximize();
-            return null;
-           }
-        mainWindow.unmaximize();
-    }
-});
-
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+ipc.on("window-status", (event, arg) => {
+  if (arg === "show") {
+    mainWindow.show();
   }
-})
+  if (arg === "minimise") {
+    mainWindow.minimize();
+  }
+  if (arg === "maximize") {
+    if (!mainWindow.isMaximized()) {
+      mainWindow.maximize();
+      return null;
+    }
+    mainWindow.unmaximize();
+  }
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
--- renderer.js
+++ renderer.js
@@ -1,44 +1,54 @@
-'use strict';
+"use strict";
 
-var ipc = require('electron').ipcRenderer;
-const process = require('child_process')
-const $ = require('jquery')
-const path = require('path')
-var moment = require('moment');  
+var ipc = require("electron").ipcRenderer;
+const process = require("child_process");
+const $ = require("jquery");
+const path = require("path");
+var moment = require("moment");
 
-const fs = require('fs')
-var {shell } = require('electron');
-var remote= require('electron').remote;
+const fs = require("fs");
+var { shell } = require("electron");
+//var remote = require("electron").remote;
 
-const Store = require('electron-store');
 getStates();
-const store = new Store();
-store.set('unicorn', '🦄');
 
-$('#minimise').on('click', e => {
-    ipc.send("window-status","minimise");
-})
-$('#maximize').on('click', e => {
-    ipc.send("window-status","maximize");
-    getData();
-})
-function getData(){
-    console.log(store.get('unicorn'));
+let tmr;
 
-    var pin = $("#pin").val();
-    $("#centers").html('');
+$("#stop").hide();
 
-    let t_date = moment().format('DD-MM-YYYY');
-    $.ajax({
-        url: "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode="+ pin + "&date=" + t_date
-    }).then(function(data) {
-        
-        $.each(data.centers, function(index, item){  
-            //console.log(item.name);  
-            let seesions = item.sessions;
-            
+$("#minimise").on("click", (e) => {
+  ipc.send("window-status", "minimise");
+});
+$("#maximize").on("click", (e) => {
+  ipc.send("window-status", "maximize");
+  getData();
+});
+getVersion();
 
-            var item_data = `<div class="card text-white bg-${item.fee_type==='Free'?'success':'secondary'} mb-2">
+async function getVersion() {
+  const ver = await ipc.invoke("get-version", "foo");
+  $("#appVersion").html(ver);
+}
+
+function getData() {
+  var pin = $("#pin").val();
+  $("#centers").html("");
+
+  let t_date = moment().format("DD-MM-YYYY");
+  $.ajax({
+    url:
+      "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode=" +
+      pin +
+      "&date=" +
+      t_date,
+  }).then(function (data) {
+    $.each(data.centers, function (index, item) {
+      //console.log(item.name);
+      let seesions = item.sessions;
+
+      var item_data = `<div class="card text-white bg-${
+        item.fee_type === "Free" ? "success" : "secondary"
+      } mb-2">
             <div class="card-header">${item.name}</div>
               
             <div class="card-body">
@@ -51,99 +61,158 @@ function getData(){
             </p>
             </div>
           </div>`;
-          $("#centers").append(item_data);
-          $("#lg_"+index).html('');
-          $.each(seesions, function(index, ses_item){  
-            let ses_item_data = `
+      $("#centers").append(item_data);
+      $("#lg_" + index).html("");
+      $.each(seesions, function (index, ses_item) {
+        let ses_item_data = `
             <li class="list-group-item">${ses_item.date}/${ses_item.min_age_limit}/${ses_item.vaccine}/${ses_item.available_capacity}</li>
             `;
-            $("#lg_"+index).append(ses_item_data);
-        });
-
-        });
+        $("#lg_" + index).append(ses_item_data);
+      });
     });
+  });
 }
 
-function getStates(){
-    var pin = $("#pin").val();
-    $("#centers").html('');
-    $.ajax({
-        url: "https://cdn-api.co-vin.in/api/v2/admin/location/states"
-    }).then(function(data) {
-        console.log(data);
-        $.each(data.states, function(index, item){  
-            var item_data = `<option value="${item.state_id}">${item.state_name}</option>
+function getStates() {
+  $("#centers").html("");
+  $.ajax({
+    url: "https://cdn-api.co-vin.in/api/v2/admin/location/states",
+  }).then(function (data) {
+    console.log(data);
+    $.each(data.states, function (index, item) {
+      var item_data = `<option value="${item.state_id}">${item.state_name}</option>
             `;
-          $("#states").append(item_data);
- 
-        });
+      $("#states").append(item_data);
     });
+  });
 }
-function getDistricts(s_id){
-    var pin = $("#pin").val();
-    $("#districts").html('');
-    $.ajax({
-        url: "https://cdn-api.co-vin.in/api/v2/admin/location/districts/" + s_id
-    }).then(function(data) {
-        console.log(data);
-        $.each(data.districts, function(index, item){  
-            var item_data = `<option value="${item.district_id}">${item.district_name}</option>
+function getDistricts() {
+  var states = $("#states").val();
+  $("#districts").html("");
+  $.ajax({
+    url: "https://cdn-api.co-vin.in/api/v2/admin/location/districts/" + states,
+  }).then(function (data) {
+    console.log(data);
+    $.each(data.districts, function (index, item) {
+      var item_data = `<option value="${item.district_id}">${item.district_name}</option>
             `;
-          $("#districts").append(item_data);
- 
-        });
+      $("#districts").append(item_data);
     });
+  });
 }
 
-function getCenters(d_id){
+async function getCenters() {
+  var dt = await ipc.invoke("get-store", "district_text");
+  var d = await ipc.invoke("get-store", "district");
+  console.log(dt);
+  var num = Math.floor(Math.random() * 1000 + 1);
+  var district_text = $("#districts").val()
+    ? $("#districts option:selected").html()
+    : dt;
 
-    let t_date = moment().format('DD-MM-YYYY');
-    $("#centers1").html('');
+  var district = $("#districts").val() ? $("#districts").val() : d;
+  $("#searching").html(district_text);
+  if (district != "") {
+    let t_date = moment().format("DD-MM-YYYY");
+    $("#centers1").html("");
     $.ajax({
-        url: "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=" + d_id + "&date=" + t_date
-    }).then(function(data) {
-        //console.log(data);
-        let filter = data.centers.map(dd => {
-            var o = new Object();
-                    
-            o.center_id = dd.center_id;
-            o.pincode = dd.pincode;
-            o.fee_type = dd.fee_type;
-            o.name = dd.name;
-            o.address = dd.address;
-            o.available_capacity = dd.sessions.reduce((s,a)=>{return s+a.available_capacity},0);
-            o.vaccine = dd.sessions.map(s=>{return s.vaccine});
-            o.fee_type = dd.fee_type;
-            //o.available_capacity = dd.available_capacity;
+      url:
+        "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=" +
+        district +
+        "&date=" +
+        t_date +
+        "&t=" +
+        num,
+    }).then(function (data) {
+      //console.log(data);
+      ipc.send("set-store", { key: "district", value: district });
+      ipc.send("set-store", { key: "district_text", value: district_text });
 
+      let filter = data.centers
+        .map((dd) => {
+          var o = new Object();
 
-            return o;
-        }).filter(data =>{
-            return data.available_capacity>0;
+          o.center_id = dd.center_id;
+          o.pincode = dd.pincode;
+          o.fee_type = dd.fee_type;
+          o.name = dd.name;
+          o.address = dd.address;
+          o.available_capacity = dd.sessions.reduce((s, a) => {
+            return s + a.available_capacity;
+          }, 0);
+          o.vaccine = dd.sessions.map((s) => {
+            return s.vaccine;
+          });
+          o.fee_type = dd.fee_type;
+          //o.available_capacity = dd.available_capacity;
+
+          return o;
+        })
+        .filter((data) => {
+          return data.available_capacity > 0;
         });
 
-        console.log(filter);
+      console.log(filter);
+      if (filter.length > 0) {
+        var myNotification = new Notification("Vaccinator ", {
+          body:
+            "Vaccination Slot available at " +
+            filter.length +
+            " Centers in " +
+            dt,
+          title: "Vaccinator Slots",
+          icon: "icon.png",
+        });
+        myNotification.onclick = () => {
+          console.log("Notification clicked");
+          NotifyStop();
+          ipc.send("window-status", "show");
+        };
 
-        $.each(filter, function(index, item){  
-            let seesions = item.sessions;
-            
-            
+        $.each(filter, function (index, item) {
+          let seesions = item.sessions;
 
-            var item_data = `<div class="card text-white bg-${item.fee_type==='Free'?'success':'secondary'} mb-2">
-            <div class="card-header">${item.name}/${item.center_id}</div>
-              
-            <div class="card-body">
-            <h5 class="card-title">Availability: ${item.available_capacity}</h5>
-            <p class="card-text">
-            ${item.address}<br>
-            ${item.pincode}
-            <small>${item.fee_type} - ${item.vaccine}</small>
-            <ul class="list-group" id="lg_${index}"></ul>
-            </p>
-            </div>
-          </div>`;
+          var item_data = `<div class="card text-white bg-${
+            item.fee_type === "Free" ? "success" : "secondary"
+          } mb-2">
+                <div class="card-header">${item.name}/${item.center_id}</div>
+                  
+                <div class="card-body">
+                <h5 class="card-title">Availability: ${
+                  item.available_capacity
+                }</h5>
+                <p class="card-text">
+                ${item.address}<br>
+                ${item.pincode}
+                <small>${item.fee_type} - ${item.vaccine}</small>
+                <ul class="list-group" id="lg_${index}"></ul>
+                </p>
+                </div>
+              </div>`;
           $("#centers1").append(item_data);
- 
         });
+      } else {
+        var item_data = `<div class="card text-white bg-danger mb-2">
+                <div class="card-header">No center Found</div>
+                  
+                <div class="card-body">
+                    <h5 class="card-title">No Slot available, check back latter.</h5>
+                </div>
+              </div>`;
+        $("#centers1").append(item_data);
+      }
     });
+  } else {
+    alert("Select District");
+  }
+}
+function NotifyStart() {
+  tmr = setInterval(getCenters, 6000);
+  $("#start").hide(200);
+  $("#stop").show(200);
+}
+function NotifyStop() {
+  clearInterval(tmr);
+  $("#start").show(200);
+  $("#stop").hide(200);
 }
